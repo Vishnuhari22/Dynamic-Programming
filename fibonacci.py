e2dbe6943e87fb80f5cad84083bddb19d74cd3cd
Vishnu Harikumar <124339928+Vishnuhari22@users.noreply.github.com>
2025-12-10 10:13:08 +0530

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,12 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        memo = {0: 0, 1: 1}
+
+        def dp(n):
+            if n in memo:
+                return memo[n]
+            else:
+                memo[n] = dp(n - 1) + dp(n - 2)
+                return memo[n]
+
+        return dp(n)
